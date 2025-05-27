f8c3365e1d86ff85cedb2d488076305ac362fa6a
LarsFlieger <a.purenetwork@gmail.com>
2025-05-27 14:08:25 +0200

    fix: rebuild to old database structure

--- src/lib/datenbank.ts
+++ src/lib/datenbank.ts
@@ -1,9 +1,8 @@
-import os from 'os'
 import path from 'path'
 import { AsyncDatabase } from 'promised-sqlite3'
 
-const datenbankPath = path.join(os.tmpdir(), 'datenbank.db')
-console.log(`Datenbankpfad: ${datenbankPath}`)
+const datenbankPath = path.join('datenbank.db')
+
 let instance: AsyncDatabase | null = null
 
 export async function getDatabase(): Promise<AsyncDatabase> {
