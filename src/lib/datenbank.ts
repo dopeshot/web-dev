e2343e9668817cd375fc4a7ac8bb58fb70575b82
LarsFlieger <a.purenetwork@gmail.com>
2025-05-27 13:56:39 +0200

    fix: tmp os

--- src/lib/datenbank.ts
+++ src/lib/datenbank.ts
@@ -1,8 +1,9 @@
+import os from 'os'
 import path from 'path'
 import { AsyncDatabase } from 'promised-sqlite3'
 
-const datenbankPath = path.join('datenbank.db')
-
+const datenbankPath = path.join(os.tmpdir(), 'datenbank.db')
+console.log(`Datenbankpfad: ${datenbankPath}`)
 let instance: AsyncDatabase | null = null
 
 export async function getDatabase(): Promise<AsyncDatabase> {
