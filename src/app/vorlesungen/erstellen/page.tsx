f1b452e90307a9f204f901e80761561bc06aaf53
LarsFlieger <a.purenetwork@gmail.com>
2025-05-27 14:28:25 +0200

    feat: add solution

--- src/app/vorlesungen/erstellen/page.tsx
+++ src/app/vorlesungen/erstellen/page.tsx
@@ -1,21 +1,52 @@
+import { getDatabase } from '@/lib/datenbank'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 export default function VorlesungErstellenPage() {
-	const save = async (formData: FormData) => {
-		// TODO: Server Actions hinzufügen um Vorlesungen in der Datenbank zu speichern
-		// TIPP: Mit formData.get('name') einzelne Formulardaten auslesen
+	async function save(formData: FormData) {
+		'use server'
 
-		// revalidatePath('/') sorgt dafür, dass die Startseite neu geladen wird
+		// Formulardaten auslesen
+		const name = formData.get('name')
+		const beschreibung = formData.get('beschreibung')
+		const dozent = formData.get('dozent')
+		const ects = formData.get('ects')
+
+		// Neue Vorlesung in der Datenbank speichern
+		const database = await getDatabase()
+		await database.run(
+			`INSERT INTO vorlesungen (name, beschreibung, dozent, ects) VALUES (?, ?, ?, ?)`,
+			[name, beschreibung, dozent, ects],
+		)
+
 		revalidatePath('/')
-		// Nach dem Speichern der Vorlesung, zur Startseite weiterleiten
 		redirect('/')
 	}
+
 	return (
 		<main className="container">
 			<h1>Vorlesung erstellen</h1>
 
-			{/* TODO: Formular erstellen */}
+			<form action={save}>
+				{/* Name der Vorlesung */}
+				<label>Name der Vorlesung</label>
+				<input type="text" name="name" />
+
+				{/* Beschreibung der Vorlesung */}
+				<label>Beschreibung</label>
+				<textarea name="beschreibung"></textarea>
+
+				{/* Name des Dozenten */}
+				<label>Name des Dozenten</label>
+				<input type="text" name="dozent" />
+
+				{/* ECTS Punkte */}
+				<label>ECTS-Punkte</label>
+				<input type="number" name="ects" />
+
+				{/* Submit Button */}
+				<button type="submit">Vorlesung erstellen</button>
+			</form>
 		</main>
 	)
 }
