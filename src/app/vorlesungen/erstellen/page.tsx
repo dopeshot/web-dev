be5327639eb3af99f65ce0aedcc6302af20e406b
LarsFlieger <a.purenetwork@gmail.com>
2025-06-03 16:37:57 +0200

    feat: finish todo

--- src/app/vorlesungen/erstellen/page.tsx
+++ src/app/vorlesungen/erstellen/page.tsx
@@ -1,10 +1,14 @@
+import { getDatabase } from '@/lib/datenbank'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 export default function VorlesungErstellenPage() {
+	// TODO: Mache aus dieser Funktion eine Server Action und vervollständige sie
 	const save = async (formData: FormData) => {
-		// TODO: Server Actions hinzufügen um Vorlesungen in der Datenbank zu speichern
-		// TIPP: Mit formData.get('name') einzelne Formulardaten auslesen
+		const name = formData.get('name')
+
+		const database = await getDatabase()
+		await database.run(`INSERT INTO vorlesungen (name) VALUES (?)`, [name])
 
 		// revalidatePath('/') sorgt dafür, dass die Startseite neu geladen wird
 		revalidatePath('/')
@@ -15,7 +19,17 @@ export default function VorlesungErstellenPage() {
 		<main className="container">
 			<h1>Vorlesung erstellen</h1>
 
-			{/* TODO: Formular erstellen */}
+			{/* TODO: save hinzufügen */}
+			<form>
+				{/* Name der Vorlesung */}
+				<label>Name der Vorlesung</label>
+				<input type="text" name="name" />
+
+				{/* TODO: Restliche Felder hinzufügen */}
+
+				{/* Submit Button */}
+				<button type="submit">Vorlesung erstellen</button>
+			</form>
 		</main>
 	)
 }
