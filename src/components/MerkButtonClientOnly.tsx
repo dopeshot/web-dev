86e14117923fc4e5c6494a4a111225ddb0d31154
LarsFlieger <a.purenetwork@gmail.com>
2025-06-03 21:20:20 +0200

    feat: improve solution

--- src/components/MerkButtonClientOnly.tsx
+++ src/components/MerkButtonClientOnly.tsx
@@ -2,14 +2,10 @@
 
 import dynamic from 'next/dynamic'
 
-const MerkButtonDynamic = dynamic(
+export const MerkButtonClientOnly = dynamic(
 	() => import('./MerkButton').then((module) => module.MerkButton),
 	{
 		ssr: false,
 		loading: () => <span aria-busy="true">Laden...</span>,
 	},
 )
-
-export const MerkButtonClientOnly: React.FC<{ id: string }> = ({ id }) => {
-	return <MerkButtonDynamic id={id} />
-}
